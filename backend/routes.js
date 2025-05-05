dcf46a0e580b6a595837f42a76a0c4361f3dc9b7
Saran0605 <saransoundharajan05@gmail.com>
2025-05-05 09:43:07 +0530

    add expenses added

--- backend/routes.js
+++ backend/routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const route = express.Router();
-const {loginUser,regUser} = require("./models.js");
+const {loginUser,regUser,addExp} = require("./models.js");
 
 route.post('/login', async (req, res) => {
   const { email, password } = req.body;
@@ -22,6 +22,18 @@ route.post('/register',async(req,res)=>{
       res.json({message:"Registered succesfully"});
     }
   }catch(err){
+    res.status(500).json("Failed");
+  }
+});
+
+route.post('/addexp',async(req,res)=>{
+  const {desc,amt,catog} = req.body;
+  try{
+    const exp = await addExp(desc,amt,catog);
+    if(exp){
+      res.json({message:"Expense added Succesfully"});
+    }
+  } catch(err){
     res.status(500).json("Failed");
   }
 });
